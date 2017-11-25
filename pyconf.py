a2cc48906cdec923f0c251e43efd3d5a54dc9435
Shuhei <sh7916@gmail.com>
2017-11-25 11:37:22 +0900

    fix globals problems occured in config file

--- pyconf.py
+++ pyconf.py
@@ -3,27 +3,54 @@
 from sys import version_info
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = ['read_config']
 
 
+def _initialize_globals(globals):
+    exec('', globals)
+
+
+def _exclude_init_globals(globals, init_globals):
+    new_globals = {}
+    for k in globals:
+        if not (k in init_globals and globals[k] is init_globals[k]):
+            new_globals[k] = globals[k]
+    return new_globals
+
+
+def _dict_intersect(a, b):  # a(dict) & b(iterable)
+    res = {}
+    for k, v in a.items():
+        if k in b:
+            res[k] = v
+    return res
+
+
 def read_config(src, required=[], default={}, exclude_unknown=True,
-                ignore_unknown=True, globals={}, exec_=None):
-    locals = {}
+                ignore_unknown=True, exclude_init_globals=True, globals={},
+                exec_=None):
+    _initialize_globals(globals)
+    init_globals = globals.copy()
+
+    # TODO configファイルが例外を送出した場合の処理
     if exec_ is None:
-        exec(src, globals, locals)
+        exec(src, globals)
     else:
-        exec_(src, globals, locals)
+        exec_(src, globals)
+
+    if exclude_init_globals:
+        globals = _exclude_init_globals(globals, init_globals)
 
     required = set(required)
-    missing_required = required - set(locals.keys())
+    missing_required = required - set(globals.keys())
     if missing_required:
         raise AttributeError(
             'missing required name(s): {}'.format(', '.join(missing_required))
         )
 
     conf = default.copy()
-    conf.update(locals)
+    conf.update(globals)
 
     known = required | set(default)
     if not ignore_unknown:
@@ -32,10 +59,6 @@ def read_config(src, required=[], default={}, exclude_unknown=True,
             raise AttributeError('given unrecognized name(s): {}'.format())
     else:
         if exclude_unknown:
-            new_conf = {}
-            for k, v in conf.items():
-                if k in known:
-                    new_conf[k] = v
-            conf = new_conf
+            conf = _dict_intersect(conf, known)
 
     return conf
